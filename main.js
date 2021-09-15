888dda1832a38d85734a5de887eab4bcea5c5ed8
Dương Văn Quang <quangbk10@gmail.com>
2021-09-15 16:43:35 +0700

    add dark mode

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const text = document.querySelector('#input')
 const btn = document.querySelector('#button')
+const darkBtn = document.querySelector('.icon-moon')
+const body = document.body
+const textarrea = document.querySelector('#input')
 btn.onclick = function(){
     const newtext = text.value.replace(/[^0-9\r?\n|\r]/g,'')
     text.value = newtext
@@ -7,4 +10,9 @@ btn.onclick = function(){
     text.select()
     text.setSelectionRange(0, 99999)
     navigator.clipboard.writeText(text.value)
+}
+
+darkBtn.onclick = function(){
+    body.classList.toggle('darkmode')
+    textarrea.classList.toggle('darkmode')
 }
